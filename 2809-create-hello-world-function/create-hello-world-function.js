0d0960b181dce8f3bbd207ab981381d577eaf236
Syed Ali Irtaza Hassan <140686714+Syed-Irtaza@users.noreply.github.com>
2024-07-30 08:30:18 +0500

    Time: 44 ms (88.04%) | Memory: 48.6 MB (68.83%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,12 @@
+/**
+ * @return {Function}
+ */
+let createHelloWorld = function () {
+    return function () {
+        return "Hello World"
+    }
+};
+
+
+const f = createHelloWorld();
+f(); // "Hello World"
